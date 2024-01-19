f87eb14100c8c088e36bce1e116d79caf0d8bd95
SOUMYAPRATAPSINGH <120254048+SOUMYAPRATAPSINGH@users.noreply.github.com>
2024-01-19 23:37:14 +0530

    routes

--- Sociohack/src/main.jsx
+++ Sociohack/src/main.jsx
@@ -5,25 +5,23 @@ import {
   BrowserRouter,
   Routes,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
-import {LoginPage} from './pages/LoginPage.jsx'
-import {SignupPage} from './pages/SignupPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
+ 
      <BrowserRouter>
         <Routes>
-            <Route path="/login" element={<LoginPage/>} />
+            <Route path="/" element={<LoginPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/" element={<App />}/>
+            <Route path="/home" element={<App />}/>
         </Routes>
       </BrowserRouter>
-    </div>
-  </div>
+   
     
   </React.StrictMode>,
 )
--- Sociohack/src/pages/LoginPage.jsx
+++ Sociohack/src/pages/LoginPage.jsx
@@ -3,7 +3,12 @@
 const LoginPage = () => {
   return (
     <div>
-      
+     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="max-w-md w-full space-y-8">
+        <h1>
+            gg
+        </h1>
+    </div></div>
     </div>
   )
 }
